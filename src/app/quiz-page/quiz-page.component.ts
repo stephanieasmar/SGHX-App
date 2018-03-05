947cccde54670efbac801a532ad59f1e42278794
Stephanie Asmar <sasmar8@gmail.com>
2018-03-05 14:01:04 -0500

    button click adds ID to the array; showMeResult can access the array

--- src/app/quiz-page/quiz-page.component.ts
+++ src/app/quiz-page/quiz-page.component.ts
@@ -11,53 +11,61 @@ export class QuizPageComponent {
   choices: any;
   isSelected: boolean;
   itemId: any;
-  // independence: boolean;
-  // contribution: boolean;
+  checkedNumber: boolean = false;
+
 
   constructor() {
     this.isSelected = false;
-    // this.independence = false;
-    // this.contribution = false;
   }
 
-  // createItemArray(itemId) {
-  //   var itemSelection = [];
-  //   itemSelection.push(itemId);
-  //   console.log(itemSelection);
-  // }
-
-
+  //DONE
   getItemClickedInfo($event) {
     var itemId = $event.target.id;
-    this.itemSelection.push(itemId);
+    this.itemSelection.push(itemId); //pushes items into
+
+    if (this.itemSelection.length >= 3) {
+      this.itemSelection.splice(3, 1); //keeps array at max 3 items
+    }
+
     console.log(this.itemSelection);
     return this.itemSelection;
   }
 
+//NEEDS WORK
   disableItem(itemId) {
     this.isSelected = true;
   }
 
+
+  //DONE
   checkNumberOfItemsSelected(itemSelection) {
-    // get 'buttons parameter' from addToButtonArray() function,
-    // check to make sure that number in buton array is equal to 3 before   
-    // enableShowMeButton() function is called. 
-
-    // if (buttons.length) == 3; {
-    //   this.enableShowMeButton();
-    // }
-
-    // else alert('Please choose 3 options')
-
+    if (this.itemSelection.length > 3) {
+      alert('Too many values chosen. Please RESET');
+    } if (this.itemSelection.length < 3) {
+      alert('Please choose THREE values');
+    } else this.checkedNumber = true;
   }
 
+
+  //DONE
   resetQuiz() {
     this.isSelected = false;
+    this.checkedNumber = false;
     this.itemSelection = [];
     console.log(this.itemSelection);
   }
 
-  showMeResult() {
-
+  navigateToArchetype() {
+    
   }
+
+//Just need to add navigation to this
+  showMeResult(itemSelection) {
+    var results = this.itemSelection;
+    this.checkNumberOfItemsSelected(results);
+    console.log(results);
+    return results;
+  }
+
+
 }
