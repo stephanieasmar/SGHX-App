92fc6e6b9858cd02f3077ca1c57c406abb9d0705
Stephanie Asmar <sasmar8@gmail.com>
2018-03-06 17:32:03 -0500

    minor changes to syntax

--- src/app/quiz-page/quiz-page.component.ts
+++ src/app/quiz-page/quiz-page.component.ts
@@ -9,6 +9,7 @@ import { ArchetypeService } from '../archetype.service';
   templateUrl: './quiz-page.component.html',
   styleUrls: ['./quiz-page.component.css']
 })
+
 export class QuizPageComponent implements OnInit {
 
   itemSelection = [];
@@ -16,9 +17,7 @@ export class QuizPageComponent implements OnInit {
   itemId: any;
   checkedNumber: boolean;
   archetypeData = [];
-  archetypes = [];
   error: any;
-
 
   constructor(private _archetypeService: ArchetypeService) {
     this.isSelected = false;
@@ -28,34 +27,21 @@ export class QuizPageComponent implements OnInit {
   ngOnInit() {
     this._archetypeService.getJSONDataAsync('../assets/data/archetypes.json').then(data => {
       this.setQueryOptionsData(data);
-      console.log(data);
+      // console.log(data);
     });
-    
   }
 
-      // var archetypes = this._archetypeService.getArchetypeData()
-    // console.log(this.archetypes);
-
-    setQueryOptionsData(data) {
-      this.archetypes = data.archetypes;
-    }
-
-
-    // this._archetypeService.getArchetypeData()
-    //   .subscribe(
-    //     archetypeData => this.archetypes = archetypeData, //success paths
-    //     error => this.error = error // error path
-    //   ); 
-    //   console.log(this.archetypeData);
-  
+  setQueryOptionsData(data) {
+    this.archetypeData = data;
+  }
 
   selectItem($event) {
     var itemId = $event.target.id;
     this.itemSelection.push(itemId); //pushes items into
+    console.log(itemId);
     if (this.itemSelection.length >= 3) {
       this.itemSelection.splice(3, 1); //keeps array at max 3 items
     }
-
     this.isSelected = true;
   }
 
@@ -65,6 +51,18 @@ export class QuizPageComponent implements OnInit {
     } if (this.itemSelection.length < 3) {
       alert('Please choose THREE values');
     } else this.checkedNumber = true;
+  }
+
+  checkIfValidOptions(itemSelection) {
+    if (this.itemSelection[0] == this.itemSelection[1]) {
+      alert('Please RESET and choose three DIFFERENT values');
+    } else if (this.itemSelection[0] == this.itemSelection[2]) {
+      alert('Please RESET and choose three DIFFERENT values');
+    }else if (this.itemSelection[1] == this.itemSelection[2]) {
+      alert('Please RESET and choose three DIFFERENT values');
+    } else if (this.itemSelection[0] == this.itemSelection[1] && (this.itemSelection[0] == this.itemSelection[2]) && (this.itemSelection[1] == this.itemSelection[2])) {
+      alert('Please RESET and choose three DIFFERENT values');
+    }
   }
 
   resetQuiz() {
@@ -80,7 +78,9 @@ export class QuizPageComponent implements OnInit {
 //Just need to add navigation to this
   showMeResult(itemSelection) {
     var results = this.itemSelection;
-    this.checkNumberOfItemsSelected(results);    
+    console.log(results);
+    this.checkNumberOfItemsSelected(results);
+    this.checkIfValidOptions(results);    
     var datas = this.archetypeData;
     for (let data in datas) {
       if(results[0]==datas[data]["First Value"]&&results[1]==datas[data]["Second Value"]&&results[2]==datas[data]["Third Value"]) {
