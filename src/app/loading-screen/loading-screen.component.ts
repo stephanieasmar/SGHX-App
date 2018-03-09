47df810d6d5dd3f69798680678618dfc2b4438c9
Stephanie Asmar <sasmar8@gmail.com>
2018-03-09 15:43:41 -0500

    loading screen works

--- src/app/loading-screen/loading-screen.component.ts
+++ src/app/loading-screen/loading-screen.component.ts
@@ -5,11 +5,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <div class="logo">
-      <div class="logo"><img src="assets/images/SGlogo.png"></div>
+      <img src="assets/images/SGlogo.png">
+      <h5>Loading...</h5>
     </div>
   </div>
   `,
-  styles: ['.container { background-color: white }', '.logo { margin-top: 40%; margin-left: 40% }']
+  styles: ['.container { background-color: white }', '.logo { margin-top: 35%; margin-left: 40% }', '.copy { font-weight: bold; margin-left: 42% }']
 })
 
 export class LoadingScreenComponent {
